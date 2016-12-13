dddd5b1734782e03e1c6c80c63369df7a818bfaf
Steven Woon <swoon@securustechnologies.com>
2016-12-13 16:31:26 -0600

    1. parser.sh now allows multiple lines with same name in a section. 2. Added sample configuration file (my.conf)

--- parser.sh
+++ parser.sh
@@ -7,8 +7,12 @@
 #   groups = admin,dev,user
 #
 #   [Servers]
-#   name = Dev
-#   status = active
+#   name = Development Plano
+#   name = Development Dallas
+#   name = Production 
+
+# There must be a space between the '=' delimiters.
+
 
 # usage: $0 config-file section key
 
@@ -19,17 +23,23 @@ ini_get () {
             $1=""
             $2=""
             sub(/^[[:space:]]+/, "")
-            print
-            exit 
+#            print $3,$NF ""
+	    for (i=3; i<=NF; ++i) printf ("%s ",$i)
+            print ""
+            next 
         }
         in_section && $1 == "" {
             # we are at a blank line without finding the var in the section
-            print "not found" > "/dev/stderr"
+            # print "not found" > "/dev/stderr"
             exit 1
         }
     ' "$1"
 }
 
 #Username=$( ini_get app.conf UserInfo name )
+#echo -e "$( ini_get $1 $2 $3 )"
+
 User=$( ini_get $1 $2 $3 )
-echo $User
+echo -e "$User"
+
+
